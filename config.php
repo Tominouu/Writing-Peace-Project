c87925efcfc6763beeea0789d5420f73937b1999
Tom Leclercq <tomleclercq2006@gmail.com>
2025-03-12 17:02:18 +0100

    add config

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+$env = parse_ini_file(__DIR__ . '/.env');
+
+try {
+    $pdo = new PDO(
+        "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8",
+        $env['DB_USER'],
+        $env['DB_PASS']
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+
+?>
